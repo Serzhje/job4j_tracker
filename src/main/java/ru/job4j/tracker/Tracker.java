99fa842130f0086111690b0ab6cb24109c17c6f7
Serzhje <7prite@gmail.com>
2021-10-20 19:37:06 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,27 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        int size = 0;
+        Item[] itemsTmp = new Item[items.length];
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] != null && items[i].getName() != null) {
+               itemsTmp[size++] = items[i];
+            }
+        }
+        return Arrays.copyOf(itemsTmp, size);
+    }
+
+    public Item[] findByName(String key) {
+        int size = 0;
+        Item[] itemsTmp = new Item[items.length];
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] != null && key.equals(items[i].getName())) {
+                itemsTmp[size++] = items[i];
+            }
+        }
+        return Arrays.copyOf(itemsTmp, size);
     }
 }
