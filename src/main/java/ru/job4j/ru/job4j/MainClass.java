aa00470defa7dcb65490d2ce0f687f8b38a483ab
Serzhje <7prite@gmail.com>
2022-08-03 19:04:17 +0300

    03.08.2022

--- src/main/java/ru/job4j/ru/job4j/MainClass.java
+++ src/main/java/ru/job4j/ru/job4j/MainClass.java
@@ -2,6 +2,6 @@ package ru.job4j.ru.job4j;
 
 public class MainClass {
     public static void main(String[] args) {
-             System.out.println("Привет!!");
+             System.out.println("Привет!!!");
     }
 }
