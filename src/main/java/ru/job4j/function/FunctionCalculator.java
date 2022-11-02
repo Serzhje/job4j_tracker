15fdc7d808ffba8fea57d58072a3573528ee18ee
Sergey Ruchin <sergey.ruchin@lamoda.ru>
2022-11-02 13:50:50 +0300

    5. Подсчет функции в диапазоне. [#24251 #357138]

--- src/main/java/ru/job4j/function/FunctionCalculator.java
+++ src/main/java/ru/job4j/function/FunctionCalculator.java
@@ -2,21 +2,15 @@ package ru.job4j.function;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.BiConsumer;
 import java.util.function.Function;
-import java.util.function.Supplier;
 
 public class FunctionCalculator {
 
     public List<Double> diapason(int start, int end, Function<Double, Double> func) {
 
-        Supplier<List<Double>> sup = ArrayList::new;
-        Function<Integer, Double> fun = Integer::doubleValue;
-        BiConsumer<List<Double>, Double> consumer = List::add;
-        List<Double> arrayList = sup.get();
+        List<Double> arrayList = new ArrayList<>();
         for (int i = start; i < end; i++) {
-
-            consumer.accept(arrayList, func.apply(fun.apply(i)));
+            arrayList.add(func.apply((double) i));
         }
         return arrayList;
     }
